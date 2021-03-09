1aeb83886edb0d5f40ffbfac5bdfe01e76cad3fe
ahayuta <forstudynb@gmail.com>
2021-03-10 02:09:16 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper-container", {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+});
